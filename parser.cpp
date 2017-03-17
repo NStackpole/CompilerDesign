f48f32f6ccf68c9ad364f52743d6b8aa901ec01e
NStackpole <nstackpole21@gmail.com>
2017-03-17 11:14:13 -0400

    Started work on parser

--- parser.cpp
+++ parser.cpp
@@ -1,3 +1,32 @@
 //Nathan Stackpole, 2017
 
-#include parser.hpp
+#include "parser.hpp"
+
+bool parser::end_of_file() const
+{
+    return first == last;
+}
+
+char parser::look_ahead() const
+{
+    if (end_of_file())
+        return 0;
+    else
+        return *first;
+}
+
+void parser::consume()
+{
+    if (end_of_file())
+        return;
+    buffer += *first;
+    ++first;
+}
+
+bool parser::match(token_kind k)
+{
+    if (look_ahead() == k)
+        consume();
+    else
+        throw("Syntax Error\n");
+}
--- parser.hpp
+++ parser.hpp
@@ -1,6 +1,21 @@
 //Nathan Stackpole, 2017
 
+#include "lexer.hpp"
+
 #ifndef PARSE_HPP
 #define PARSE_HPP
+
+class parser
+{
+    char *first;
+    char *last;
+    std::string buffer;
+
+  public:
+    bool match(token_kind k);
+    void consume();
+    char look_ahead() const;
+    bool end_of_file() const;
+};
 
 #endif
