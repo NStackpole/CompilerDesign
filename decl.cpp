7c5d506143132c87a455bb7e2720a6f443754ee9
NStackpole <nstackpole21@gmail.com>
2017-04-25 10:59:42 -0400

    Created decl and var_decl class

--- decl.cpp
+++ decl.cpp
@@ -1,3 +1,6 @@
 //Nathan Stackpole, 2017
 
 #include "decl.hpp"
+
+var_decl::var_decl(symbol *n, type *t): name(n), typ(t), init(nullptr)
+{}
--- decl.hpp
+++ decl.hpp
@@ -1,6 +1,24 @@
 //Nathan Stackpole, 2017
 
+#include <vector>
+#include "AST.hpp"
+#include "symbol.hpp"
+
 #ifndef DECL_HPP
 #define DECL_HPP
+
+struct decl
+{
+    virtual ~decl() = default;
+};
+
+struct var_decl : decl
+{
+    var_decl(symbol *n, type *t);
+
+    symbol *name;
+    type *typ;
+    expr *init;
+};
 
 #endif
