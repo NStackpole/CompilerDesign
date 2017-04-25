95eb436d90a0e99f8634b85675f32334ee72f7ee
NStackpole <nstackpole21@gmail.com>
2017-04-25 11:00:08 -0400

    Created statement, expr_statement, and decl_statement classes

--- statement.cpp
+++ statement.cpp
@@ -1,3 +1,9 @@
 //Nathan Stackpole, 2017
 
 #include "statement.hpp"
+
+expr_statement::expr_statement(expr *e) : expression(e) 
+{}
+
+decl_statement::decl_statement(decl *d) : entity(d)
+{}
--- statement.hpp
+++ statement.hpp
@@ -1,6 +1,28 @@
 //Nathan Stackpole, 2017
 
+#include "decl.hpp"
+#include "AST.hpp"
+
 #ifndef STMT_HPP
 #define STMT_HPP
+
+struct statement
+{
+    virtual ~statement() = default;
+};
+
+struct expr_statement : statement
+{
+    expr_statement(expr *);
+
+    expr *expression;
+};
+
+struct decl_statement : statement
+{
+    decl_statement(decl *);
+
+    decl *entity;
+};
 
 #endif
