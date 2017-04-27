40cd936c0f610446d044953858521ebdd4bc8803
NStackpole <nstackpole21@gmail.com>
2017-04-27 10:58:22 -0400

    Implemented scope class with find and insert functions

--- parser/scope.cpp
+++ parser/scope.cpp
@@ -1,3 +1,17 @@
 //Nathan Stackpole, 2017
 
 #include "scope.hpp"
+
+void scope::insert(symbol *s, decl *d)
+{
+    map.insert({s, d});
+}
+
+decl *scope::find(symbol *s)
+{
+    auto iter = map.find(s);
+    if(iter != map.end())
+        return iter->second;
+    else
+        return nullptr;
+}
--- parser/scope.hpp
+++ parser/scope.hpp
@@ -1,6 +1,21 @@
 //Nathan Stackpole, 2017
 
+#include "decl.hpp"
+#include "../lexer/symbol.hpp"
+
+#include <unordered_map>
+
 #ifndef SCOPE_HPP
 #define SCOPE_HPP
+
+struct scope
+{
+    void insert(symbol*, decl*);
+
+    decl* find(symbol*);
+
+    std::unordered_map<symbol*, decl*> map;
+};
+
 
 #endif
