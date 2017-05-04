cddc1ebe0ad4e9a320c70a1b6e22b6eda22bfe62
NStackpole <nstackpole21@gmail.com>
2017-05-04 12:41:17 -0400

    put in header guard

--- codegen/codegen.hpp
+++ codegen/codegen.hpp
@@ -3,6 +3,11 @@
 #include "../parser/parser.hpp"
 #include "../lexer/lexer.hpp"
 
+#ifndef CODEGEN_HPP
+#define CODEGEN_HPP
+
 std::vector<token *> lex(char *, keyword_table *, symbol_table *);
 
 expr *parse(std::vector<token *>, symbol_table *, std::deque<scope *> &);
+
+#endif
